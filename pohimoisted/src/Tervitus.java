86a3886dd2a780408c45801c02c3a92eb9456dfe
karolin.laas <karolinlaas@hotmail.com>
2018-11-14 09:57:05 +0200

    Loodud kasutaja tervitus

--- pohimoisted/src/Tervitus.java
+++ pohimoisted/src/Tervitus.java
@@ -4,6 +4,7 @@ public class Tervitus {
     public static void tervita() {
         System.out.println(tervitusTekst);
     }
+    
 
     public static void main(String[] args) {
         tervitusTekst = "Tere Karolin!";
