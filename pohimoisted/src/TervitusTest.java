8c81251574219661973734f6ff9fa9853f7db6ab
karolin.laas <karolinlaas@hotmail.com>
2018-11-15 09:43:08 +0200

    Loodud Tervitus tüüpi konstruktor, mis võimaldab määrata Tervitus sõnumit

---
+++ pohimoisted/src/TervitusTest.java
@@ -0,0 +1,6 @@
+public class TervitusTest {
+    public static void main(String[] args) {
+        Tervitus karolinTervitus = new Tervitus("Tere, õpilased!");
+        karolinTervitus.tervita();
+    }
+}
