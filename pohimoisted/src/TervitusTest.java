1df833adce85271a0fdb07279e5fdf385f7256bb
karolin.laas <karolinlaas@hotmail.com>
2018-11-15 10:02:27 +0200

    Uus Tervitus tüüpi konstruktor, võimaldab tühja tervitust

--- pohimoisted/src/TervitusTest.java
+++ pohimoisted/src/TervitusTest.java
@@ -1,6 +1,8 @@
 public class TervitusTest {
     public static void main(String[] args) {
         Tervitus karolinTervitus = new Tervitus("Tere, õpilased!");
+        Tervitus vaikneTervitus = new Tervitus();
         karolinTervitus.tervita();
+        vaikneTervitus.tervita();
     }
 }
