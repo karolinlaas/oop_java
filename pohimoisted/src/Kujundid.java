c7b6f67b73fdbfefca0a92f0c0e65acb5a0b9e5d
karolin.laas <karolinlaas@hotmail.com>
2018-11-22 10:32:26 +0200

    Täiendatud Kujund klass nimetuse nurkadeArv ja nurkadeOlemasolu setteritega ning Ristkuliku klass lisaomadustega

--- pohimoisted/src/Kujundid.java
+++ pohimoisted/src/Kujundid.java
@@ -1,19 +1,22 @@
 public class Kujundid {
     public static void main(String[] args) {
-        Kujund ring = new Kujund("Ring");
-        Kujund ruut = new Kujund("Ruut");
-        Kujund kolmnurk = new Kujund("Kolmnurk");
+//        Kujund ring = new Kujund("Ring");
+//        Kujund ruut = new Kujund("Ruut");
+//        Kujund kolmnurk = new Kujund("Kolmnurk");
 
-        ring.valjastaKirjeldus();
+//        ring.valjastaKirjeldus();
 
 /*      Need read ei tööta, kuna nimetus on privaatne omadus
         ring.nimetus = "Ruut";
          ring.valjastaKirjeldus(); */
 
-        ring.setNimetus("Ruut");
-        ring.valjastaKirjeldus();
+//        ring.setNimetus("Ruut");
+//        ring.valjastaKirjeldus();
 
-        ruut.valjastaKirjeldus();
-        kolmnurk.valjastaKirjeldus();
+//        ruut.valjastaKirjeldus();
+//        kolmnurk.valjastaKirjeldus();
+
+        Ristkulik ristkulik = new Ristkulik("Ristkülik");
+        ristkulik.valjastaKirjeldus();
     }
 }
---
+++ pohimoisted/src/Ristkulik.java
@@ -0,0 +1,14 @@
+public class Ristkulik extends Kujund {
+//  objekti omadused - klassi muutujad
+    private Double pikkus;
+    private Double laius;
+    private Integer nurk;
+
+    public Ristkulik(String nimi, Double p, Double l) {
+        super(nimi);
+        this.pikkus = p;
+        this.laius = l;
+        this.nurk = 90;
+        System.out.println("Ristküliku klassi konstruktor");
+    }
+}
