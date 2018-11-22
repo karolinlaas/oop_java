5e9b8b4798ef43d50c7cffbf03612112612848e7
karolin.laas <karolinlaas@hotmail.com>
2018-11-22 10:59:16 +0200

    Kujund.java's on getterid, Ristkulik.java's on omaduste väljastamine

--- pohimoisted/src/Ristkulik.java
+++ pohimoisted/src/Ristkulik.java
@@ -8,7 +8,21 @@ public class Ristkulik extends Kujund {
         super(nimi);
         this.pikkus = p;
         this.laius = l;
+        this.setNurkadeOlemasolu(true);
+        this.setNurkadeArv(4);
         this.nurk = 90;
         System.out.println("Ristküliku klassi konstruktor");
+    }
+
+    @Override
+    public void valjastaKirjeldus() {
+        System.out.println("Ristküliku klassi valjastaKirjeldus()");
+        System.out.println(this.getNimetus());
+        System.out.println("Pikkus = " + this.pikkus);
+        System.out.println("Laius = " + this.laius);
+        if(this.getNurkadeOlemasolu()) {
+            System.out.println("Kokku on " + this.getNurkadeArv() + " nurka");
+        }
+        System.out.println("Nurgad on " + this.nurk + " kraadi.");
     }
 }
