5437ef543e25482b62d52b6cc53a9ca6ba1367a6
karolin.laas <karolinlaas@hotmail.com>
2018-11-29 09:56:42 +0200

    Printeri kirjeldus ja test

---
+++ pohimoisted/src/PrinteriTest.java
@@ -0,0 +1,10 @@
+public class PrinteriTest {
+    public static void main(String[] args) {
+        Printer printer1 = new Printer(50, false);
+        printer1.lisaTooner(15);
+        System.out.println("Printeri tooneri tase on " + printer1.getTooneriTase() + "%");
+
+        printer1.lisaTooner(50);
+        System.out.println("Printeri tooneri tase on " + printer1.getTooneriTase() + "%");
+    }
+}
